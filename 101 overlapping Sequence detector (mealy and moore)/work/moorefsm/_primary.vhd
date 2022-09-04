43b2cbb6586b761f2d4595c765fe00cc588ac9c1
thanavignesh <83361794+thanavignesh@users.noreply.github.com>
2022-09-04 11:07:39 +0530

    Add files via upload

---
+++ 101 overlapping Sequence detector (mealy and moore)/work/moorefsm/_primary.vhd
@@ -0,0 +1,21 @@
+library verilog;
+use verilog.vl_types.all;
+entity moorefsm is
+    generic(
+        s0              : vl_logic_vector(1 downto 0) := (Hi0, Hi0);
+        s1              : vl_logic_vector(1 downto 0) := (Hi0, Hi1);
+        s2              : vl_logic_vector(1 downto 0) := (Hi1, Hi0);
+        s3              : vl_logic_vector(1 downto 0) := (Hi1, Hi1)
+    );
+    port(
+        \in\            : in     vl_logic;
+        clk             : in     vl_logic;
+        rst             : in     vl_logic;
+        \out\           : out    vl_logic
+    );
+    attribute mti_svvh_generic_type : integer;
+    attribute mti_svvh_generic_type of s0 : constant is 2;
+    attribute mti_svvh_generic_type of s1 : constant is 2;
+    attribute mti_svvh_generic_type of s2 : constant is 2;
+    attribute mti_svvh_generic_type of s3 : constant is 2;
+end moorefsm;
